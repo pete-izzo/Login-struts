33fe89089e73e92729f54c147131b75ef1c99aff
Peter Izzo <pizzo@avantus.com>
2021-02-01 17:09:39 -0500

    create dispatch action

---
+++ src/mine/actions/OrderAction.java
@@ -0,0 +1,73 @@
+package mine.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSetMetaData;
+import javax.sql.DataSource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+import java.lang.*;
+import static java.lang.System.*;
+import java.lang.Object;
+import java.util.Collections;
+import static java.util.Comparator.comparing;
+import java.io.*;
+import java.util.*;
+
+import org.apache.struts.action.*;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import org.apache.struts.action.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+import mine.formbeans.LoginForm;
+
+
+public class UserAction extends DispatchAction {
+
+    private final static String SUCCESS = "success";
+
+    //add order
+    public ActionForward add(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        UserForm userForm = (UserForm) form;
+        userForm.setMessage("Inside add user method.");
+        return mapping.findForward(SUCCESS);
+    }
+
+    //edit orders
+    public ActionForward update(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        UserForm userForm = (UserForm) form;
+        userForm.setMessage("Inside update user method.");
+        return mapping.findForward(SUCCESS);
+    }
+
+    //delete order
+    public ActionForward delete(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        UserForm userForm = (UserForm) form;
+        userForm.setMessage("Inside delete user method.");
+        return mapping.findForward(SUCCESS);
+    }
+}
